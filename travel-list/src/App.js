a5b9df5034d45db26a43dc84ad8fbdcd04814d8e
oladotun <oladotunadekomaya@gmail.com>
2024-11-19 10:24:59 +0100

    commit

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,9 +1,61 @@
-export default function App() {}
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Boxers", quantity: 9, packed: true },
+  { id: 2, description: "Milo", quantity: 7, packed: false },
+  { id: 2, description: "shoes", quantity: 10, packed: true },
+];
 
-function Logo() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-function Form() {}
+function Logo() {
+  return <h1> Far Away</h1>;
+}
 
-function PackingList() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip</h3>
+    </div>
+  );
+}
 
-function Stats() {}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => {
+          return <Item item={item} />;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button style={{ color: "red" }}>&times;</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
