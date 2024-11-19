54c92a0310601df998e6623cc88fff8adc462bf7
oladotun <oladotunadekomaya@gmail.com>
2024-11-19 15:02:38 +0100

    commit

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -9,11 +9,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => setItems([...items, item]);
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -23,7 +27,7 @@ function Logo() {
   return <h1> Far Away</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -38,6 +42,8 @@ function Form() {
       packed: false,
       id: Date.now(),
     };
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -64,11 +70,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => {
+        {items.map((item) => {
           return <Item item={item} key={item.id} />;
         })}
       </ul>
