5eb24fa87ea756889c302a4e4de420838f7a1cb6
oladotun <oladotunadekomaya@gmail.com>
2024-11-20 19:30:17 +0100

    commit

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -133,35 +133,6 @@ function PackingList({ items, onDeleteItems, onToggleCheck }) {
       </div>
     </div>
   );
-
-  return (
-    <div className="list">
-      <ul>
-        {items.map((item) => {
-          return (
-            <Item
-              item={item}
-              key={item.id}
-              onDeleteItems={onDeleteItems}
-              onToggleCheck={onToggleCheck}
-            />
-          );
-        })}
-      </ul>
-      <div className="actions">
-        <select
-          name=""
-          id=""
-          value={sortValue}
-          onChange={(e) => setSortValue(e.target.value)}
-        >
-          <option value="chronological">Sort by Item Added</option>
-          <option value="alphabetical">Sort Alphabetically</option>
-          <option value="bypacked"> Sort by Packed</option>
-        </select>
-      </div>
-    </div>
-  );
 }
 
 function Item({ item, onDeleteItems, onToggleCheck }) {
