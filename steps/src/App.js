2b0d471a75ac3f34b3f2454be840f95ce7a021a2
oladotun <oladotunadekomaya@gmail.com>
2024-11-21 08:30:49 +0100

    commit

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,65 +1,65 @@
 import { useState } from "react";
 
-// const messages = [
-//   "Learn React ⚛️",
-//   "Apply for jobs 💼",
-//   "Invest your new income 🤑",
-// ];
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
-// export default function App() {
-//   const [step, setStep] = useState(1);
-//   const [isOpen, setIsOpen] = useState(true);
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
-//   const handlePrevious = () => {
-//     if (step > 1) {
-//       setStep((step) => step - 1);
-//     }
-//   };
-//   const handleNext = () => {
-//     if (step < 3) setStep((step) => step + 1);
-//   };
+  const handlePrevious = () => {
+    if (step > 1) {
+      setStep((step) => step - 1);
+    }
+  };
+  const handleNext = () => {
+    if (step < 3) setStep((step) => step + 1);
+  };
 
-//   const handleIsOpen = () => {
-//     // isOpen ? setIsOpen(false) : setIsOpen(true);
-//     setIsOpen((open) => !open);
-//   };
+  const handleIsOpen = () => {
+    // isOpen ? setIsOpen(false) : setIsOpen(true);
+    setIsOpen((open) => !open);
+  };
 
-//   return (
-//     <>
-//       <button className="close" onClick={handleIsOpen}>
-//         &times;
-//       </button>
-//       {isOpen ? (
-//         <div className="steps">
-//           <div className="numbers">
-//             <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-//             <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-//             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-//           </div>
+  return (
+    <>
+      <button className="close" onClick={handleIsOpen}>
+        &times;
+      </button>
+      {isOpen ? (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-//           <div className="message">
-//             Step {step}: {messages[step - 1]}
-//           </div>
+          <div className="message">
+            Step {step}: {messages[step - 1]}
+          </div>
 
-//           <div className="buttons">
-//             <button
-//               style={{ backgroundColor: "#7950f2", color: "#fff" }}
-//               onClick={handlePrevious}
-//             >
-//               Previous
-//             </button>
-//             <button
-//               style={{ backgroundColor: "#7950f2", color: "#fff" }}
-//               onClick={handleNext}
-//             >
-//               Next
-//             </button>
-//           </div>
-//         </div>
-//       ) : null}
-//     </>
-//   );
-// }
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+}
 
 // export default function App() {
 //   const [count, setCount] = useState(0);
