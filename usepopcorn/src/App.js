86029e8858e02eea5d9ed73f2486bc835deccaab
oladotun <oladotunadekomaya@gmail.com>
2024-11-22 21:16:43 +0100

    commit

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -91,24 +91,7 @@ export default function App() {
             <>
               <ul className="list">
                 {watched.map((movie) => (
-                  <li key={movie.imdbID}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <h3>{movie.Title}</h3>
-                    <div>
-                      <p>
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                      </p>
-                      <p>
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                      </p>
-                      <p>
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                      </p>
-                    </div>
-                  </li>
+                  <WatchedMoviesList movie={movie} />
                 ))}
               </ul>
             </>
@@ -182,5 +165,28 @@ function WatchedMoviesSummary({ watched }) {
         </p>
       </div>
     </div>
+  );
+}
+
+function WatchedMoviesList({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
   );
 }
