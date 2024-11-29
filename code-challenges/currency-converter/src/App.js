0a183b2f359e6ddd7c02df68910fc8e3ffebc94b
oladotun <oladotunadekomaya@gmail.com>
2024-11-29 23:48:42 +0100

    commit

--- code-challenges/currency-converter/src/App.js
+++ code-challenges/currency-converter/src/App.js
@@ -1,16 +1,36 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useState } from "react";
+
 export default function App() {
+  const [baseCurrency, setBaseCurrency] = useState("");
+  const [foreignCurrency, setForeignCurrency] = useState("");
+
+  console.log("base currency:", baseCurrency);
+  console.log("foreign currency:", foreignCurrency);
+
+  function handleChangeCurrency(setterFunc, value) {
+    setterFunc(value);
+  }
+
   return (
     <div>
       <input type="text" />
-      <select onChange={(e) => console.log(e.target.value)}>
+      <select
+        onChange={(e) => handleChangeCurrency(setBaseCurrency, e.target.value)}
+        value="USD"
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select onChange={(e) => console.log(e.target.value)}>
+      <select
+        onChange={(e) =>
+          handleChangeCurrency(setForeignCurrency, e.target.value)
+        }
+        value="EUR"
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
