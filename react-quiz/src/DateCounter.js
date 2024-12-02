b571453a769e82f445ffd099c31622c406104cf5
oladotun <oladotunadekomaya@gmail.com>
2024-12-02 10:44:40 +0100

    commit

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -31,7 +31,7 @@ function DateCounter() {
   };
 
   const defineCount = function (e) {
-    // dispatch({type: "count", payload: })
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
   };
 
