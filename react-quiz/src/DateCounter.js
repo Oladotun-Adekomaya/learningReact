999aefc7fdfe6c695d9f83b6f3ea4b8cc8b7eb32
oladotun <oladotunadekomaya@gmail.com>
2024-12-02 11:11:42 +0100

    commit

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -2,17 +2,18 @@ import { useState, useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + action.payload;
-  if (action.type === "dec") return state + action.payload;
-  if (action.type === "setCount") return action.payload;
+  // if (action.type === "inc") return state + action.payload;
+  // if (action.type === "dec") return state + action.payload;
+  // if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
 
-  const [count, dispatch] = useReducer(reducer, 0);
-
-  const [step, setStep] = useState(1);
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -36,12 +37,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
@@ -51,10 +52,10 @@ function DateCounter() {
           type="range"
           min="0"
           max="10"
-          value={step}
+          // value={step}
           onChange={defineStep}
         />
-        <span>{step}</span>
+        {/* <span>{step}</span> */}
       </div>
 
       <div>
