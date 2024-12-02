df24f599b58a8d40feada8af39dddda865886ebd
oladotun <oladotunadekomaya@gmail.com>
2024-12-02 22:45:50 +0100

    commit

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,12 @@
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
+
+      <Main></Main>
     </div>
   );
 }
