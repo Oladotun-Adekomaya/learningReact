d9e8ed2195780f9836dcd6cd8fc9db2dc41da6e8
oladotun <oladotunadekomaya@gmail.com>
2024-12-02 22:26:52 +0100

    commit

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,10 +1,5 @@
 import DateCounter from "./DateCounter";
 
 export default function App() {
-  return (
-    <div>
-      {" "}
-      <DateCounter />
-    </div>
-  );
+  return <div className="app"></div>;
 }
