02576ac2efa727b7e29d5873d9d396b42bdcb751
oladotun <oladotunadekomaya@gmail.com>
2024-12-02 23:30:14 +0100

    commit

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,12 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (key) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error", err));
   }, []);
 
   return (
