38664cd913eb58d5a0f5cff3be82b24fb2ba8f68
oladotun <oladotunadekomaya@gmail.com>
2024-12-03 00:29:01 +0100

    commit

--- react-quiz/src/StartScreen.js
+++ react-quiz/src/StartScreen.js
@@ -2,7 +2,7 @@ function StartScreen({ numQuestions }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz</h2>
-      <h3>X question to test your React mastery</h3>
+      <h3>{numQuestions} question to test your React mastery</h3>
       <button>Let's start</button>
     </div>
   );
