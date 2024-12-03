e7e71b672069c7c3f8c06253a357ecdd20af619f
oladotun <oladotunadekomaya@gmail.com>
2024-12-03 01:39:47 +0100

    commit

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useReducer } from "react";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Loader from "./components/Loader";
-import Error from "./components/Error";
-import StartScreen from "./components/StartScreen";
-import Question from "./components/Question";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
