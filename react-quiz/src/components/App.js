f7cb32eb1bdf58f968dd84621d56bc7cd23d1923
oladotun <oladotunadekomaya@gmail.com>
2024-12-03 02:36:10 +0100

    commit

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -87,7 +87,7 @@ export default function App() {
               answer={answer}
             />
 
-            <NextButton dispatch={dispatch} />
+            <NextButton dispatch={dispatch} answer={answer} />
           </>
         )}
       </Main>
--- react-quiz/src/components/NextButton.js
+++ react-quiz/src/components/NextButton.js
@@ -1,4 +1,4 @@
-function NextButton() {
+function NextButton({ dispatch, answer }) {
   return <div></div>;
 }
 
