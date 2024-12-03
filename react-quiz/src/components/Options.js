d4edc8cda076b1fe40cdabd831498de4ccaf1404
oladotun <oladotunadekomaya@gmail.com>
2024-12-03 01:36:50 +0100

    commit

---
+++ react-quiz/src/components/Options.js
@@ -0,0 +1,14 @@
+function Options() {
+  return (
+    <div className="options">
+      {question.options.map((option) => (
+        <button className="btn btn-option" key={option}>
+          {" "}
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
