4ae5f9723aeea3d4490a7f06d89cd11b3bde8a1d
oladotun <oladotunadekomaya@gmail.com>
2024-12-04 12:24:26 +0100

    commit

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div>World wise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
