a8b6102919265bfc60f75fe52aca8c4dcee1dbe0
oladotun <oladotunadekomaya@gmail.com>
2024-12-04 12:45:36 +0100

    commit

--- worldwise/src/pages/HomePage.jsx
+++ worldwise/src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function HomePage() {
-  return <div>Worldwise</div>;
+  return (
+    <div>
+      <h1>Worldwise</h1>
+
+      <PageNav />
+    </div>
+  );
 }
 
 export default HomePage;
