b1d699ab54fcb57e634cb6a6bf3fdf9bf3c06f4d
oladotun <oladotunadekomaya@gmail.com>
2024-12-06 17:55:13 +0100

    commit

--- worldwise/src/components/PageNav.jsx
+++ worldwise/src/components/PageNav.jsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
