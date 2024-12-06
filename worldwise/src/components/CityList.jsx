213556a495bc7ff7ae3e6e68b0a2c933d833b76e
oladotun <oladotunadekomaya@gmail.com>
2024-12-06 20:21:29 +0100

    commit

---
+++ worldwise/src/components/CityList.jsx
@@ -0,0 +1,5 @@
+function CityList() {
+  return <ul></ul>;
+}
+
+export default CityList;
--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
