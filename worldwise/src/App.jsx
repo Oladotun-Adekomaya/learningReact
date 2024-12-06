a0dbfde01f2dc83edcfba200af1936ae2e8c294e
oladotun <oladotunadekomaya@gmail.com>
2024-12-06 18:09:52 +0100

    commit

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -10,12 +10,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<HomePage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
