13aee273db4dbdc27f1dec5c832f6904c2c2d77c
oladotun <oladotunadekomaya@gmail.com>
2024-12-06 21:57:44 +0100

    commit

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -6,8 +6,32 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:3000";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`); // , { mode: "no-cors" }
+        const data = await res.json();
+        setCities(data);
+      } catch (e) {
+        alert("There was an error");
+        console.error(e);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
