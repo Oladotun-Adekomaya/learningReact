9a8f48dbbb351b8ac281c3e4058a11f26f0b50d1
oladotun <oladotunadekomaya@gmail.com>
2024-12-11 10:22:02 +0100

    commit

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -1,11 +1,17 @@
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
-import CityItem from "./CityItem"
+import CityItem from "./CityItem";
 
 function CityList({ isLoading, cities }) {
   if (isLoading) return <Spinner />;
 
-  return <ul className={styles.cityList}>{cities.map((city) => )}</ul>;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
