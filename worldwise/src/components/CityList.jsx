17b4a560541d83fcca1f7d234af246ab4b6de4b2
oladotun <oladotunadekomaya@gmail.com>
2024-12-14 06:05:47 +0100

    commit

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -1,9 +1,15 @@
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
+import Message from "./Message";
 
 function CityList({ isLoading, cities }) {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city in the map" />
+    );
 
   return (
     <ul className={styles.cityList}>
