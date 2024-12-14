1490f3f905a25ace52e539faf9f81c555e927290
oladotun <oladotunadekomaya@gmail.com>
2024-12-14 16:52:42 +0100

    commit

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -50,10 +50,7 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          ></Route>
+          <Route index element={<Navigate to="cities" replace />}></Route>
 
           <Route
             path="cities"
