bd3a5c0fc78cb5a085c45af68a44ba6184532136
oladotun <oladotunadekomaya@gmail.com>
2024-12-15 23:15:10 +0100

    commit

--- atomic-blog/src/PostContext.js
+++ atomic-blog/src/PostContext.js
@@ -1,6 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 // 1) CREATE A CONTEXT
 const PostContext = createContext();
 
-function PostProvider(params) {}
+function PostProvider() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+}
