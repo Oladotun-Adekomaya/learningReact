1203cdbfad940d07f9d7ddce25549c14766a71ef
oladotun <oladotunadekomaya@gmail.com>
2024-12-16 10:00:37 +0100

    commit

--- atomic-blog/src/PostContext.js
+++ atomic-blog/src/PostContext.js
@@ -52,6 +52,9 @@ function PostProvider({ children }) {
 
 function usePosts() {
   const context = useContext(PostContext);
+  if (context === undefined) {
+    throw new Error("Postcontext was used outside of the PostProvider");
+  }
   return context;
 }
 
